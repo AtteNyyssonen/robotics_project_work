895f7c1c0afbb1d7459d0111d3958e385cb3810e
Atte <atte.nyyssonen@gmail.com>
2025-10-01 19:14:06 +0300

    Run simulation and spawn table

--- dual_arm_robot_setup/src/fr3_dual_arm_sim/launch/simulation.launch.py
+++ dual_arm_robot_setup/src/fr3_dual_arm_sim/launch/simulation.launch.py
@@ -1,34 +1,29 @@
-import os
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
-from launch.event_handlers import OnProcessExit
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
     pkg = get_package_share_directory('fr3_dual_arm_sim')
-
     world = os.path.join(pkg, 'worlds', 'simulation_world.sdf')
-
-    urdf_path = os.path.join(pkg, 'urdf', 'dual_fr3.xacro')
-
+    urdf = os.path.join(pkg, 'urdf', 'dual_fr3.xacro')
 
     return LaunchDescription([
+        # Launch Gazebo Sim
         ExecuteProcess(
-            cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_factory.so', '-s', 'libgazebo_ros_init.so'],
+            cmd=['gz', 'sim', '-v', '4', world],
             output='screen'
         ),
 
-        Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            arguments=[
-              '-entity', 'dual_fr3',
-              '-file', urdf_path,
-              '-topic', 'robot_description',
-              '-x', '0', '-y', '0', '-z', '0'
-            ],
-            output='screen'
-        ),
+        # Spawn the robot entity into Gazebo Sim
+        #Node(
+            #package='ros_gz_sim',
+            #executable='create',
+            #arguments=[
+                #'-name', 'dual_fr3',
+                #'-file', urdf
+            #],
+            #output='screen'
+        #),
     ])
