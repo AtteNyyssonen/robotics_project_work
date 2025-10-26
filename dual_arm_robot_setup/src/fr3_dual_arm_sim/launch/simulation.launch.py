1dcfe1b2f612a190db0efc732bde6747787b4cb6
Atte <atte.nyyssonen@gmail.com>
2025-10-26 13:41:05 +0200

    Additions regarding moveit integration

--- dual_arm_robot_setup/src/fr3_dual_arm_sim/launch/simulation.launch.py
+++ dual_arm_robot_setup/src/fr3_dual_arm_sim/launch/simulation.launch.py
@@ -6,11 +6,14 @@ from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node, PushRosNamespace
+from launch_ros.parameter_descriptions import ParameterValue
 import xacro
+import yaml
 
 def generate_launch_description():
     pkg_my_robot_description = get_package_share_directory('fr3_dual_arm_sim')
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    pkg_robot_moveit = get_package_share_directory('fr3_dual_arm_moveit_config')
 
     world_path_arg = DeclareLaunchArgument(
         'world_path',
@@ -18,12 +21,10 @@ def generate_launch_description():
         description='Path to the Gazebo world file (.sdf)'
     )
 
-    # --- Robot Description ---
     xacro_file = os.path.join(pkg_my_robot_description, 'urdf', 'dual_fr3.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     robot_description = {'robot_description': robot_description_config.toxml()}
 
-    # --- Gazebo Sim ---
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
@@ -31,7 +32,6 @@ def generate_launch_description():
         launch_arguments={'gz_args': [LaunchConfiguration('world_path'), ' -r -v 4']}.items(),
     )
 
-    # --- Core Nodes ---
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -46,7 +46,6 @@ def generate_launch_description():
         output='screen'
     )
 
-    # --- ROS-Gazebo Bridge ---
     gz_bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
@@ -54,13 +53,37 @@ def generate_launch_description():
             '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
             '/world/dual_fr3/joint_state_broadcaster/joint_states@sensor_msgs/msg/JointState[gz.msgs.Model',
         ],
+        remappings=[('/world/dual_fr3/joint_state_broadcaster/joint_states', '/joint_states')],
         output='screen'
     )
+
+    srdf_path = os.path.join(pkg_robot_moveit, 'config', 'dual_fr3.srdf')
+    with open(srdf_path, 'r') as srdf_file:
+        robot_description_semantic = {'robot_description_semantic': srdf_file.read()}
     
+    kinematics_path = os.path.join(pkg_robot_moveit, 'config', 'kinematics.yaml')
+    with open(kinematics_path, 'r') as kin_file:
+        kinematics_yaml = yaml.safe_load(kin_file)
+    robot_description_kinematics = {'robot_description_kinematics': kinematics_yaml}
+
+    move_group_node = Node(
+        package='moveit_ros_move_group',
+        executable='move_group',
+        output='screen',
+        parameters=[
+            robot_description,
+            robot_description_semantic,
+             robot_description_kinematics,
+            {'use_sim_time': True},
+            os.path.join(pkg_robot_moveit, 'config', 'moveit_controllers.yaml'),
+        ],
+    )
+
     return LaunchDescription([
         world_path_arg,
         gz_sim,
         gz_bridge,
         robot_state_publisher,
         spawn_entity,
+        move_group_node
     ])
