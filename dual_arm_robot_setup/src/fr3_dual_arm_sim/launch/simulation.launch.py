5711384f346ff96c119531ec7532b025de80731f
Atte <atte.nyyssonen@gmail.com>
2025-11-01 12:27:28 +0200

    remove references to table model

--- dual_arm_robot_setup/src/fr3_dual_arm_sim/launch/simulation.launch.py
+++ dual_arm_robot_setup/src/fr3_dual_arm_sim/launch/simulation.launch.py
@@ -20,7 +20,7 @@ def generate_launch_description():
         description='Path to the Gazebo world file (.sdf)'
     )
 
-    xacro_file = os.path.join(pkg_my_robot_description, 'urdf', 'dual_fr3.xacro')
+    xacro_file = os.path.join(pkg_my_robot_description, 'urdf', 'dual_fr3.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     robot_description = {'robot_description': robot_description_config.toxml()}
 
